06d9f2a30625669916eb2d7bd4ff61c3e3d26457
Arnold Williams <arnoldwilliams@hotmail.com>
2024-02-01 18:52:47 +0000

    Model field added to Products table.

--- SqlServerDatabase/Product.sql
+++ SqlServerDatabase/Product.sql
@@ -4,5 +4,6 @@
     [Name] VARCHAR(50) NULL, 
     [Cost] INT NULL, 
     [Color] NVARCHAR(50) NULL, 
-    [Description] NVARCHAR(50) NULL
+    [Description] NVARCHAR(50) NULL, 
+    [Model] NCHAR(30) NULL
 )
